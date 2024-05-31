c34410ec53872ee4e3372549d28ecb2120bd8bcb
Habababa17 <98645316+Habababa17@users.noreply.github.com>
2024-05-31 14:30:10 +0200

    Add files via upload

---
+++ example configs/config2.py
@@ -0,0 +1,3 @@
+# Raspberry Pi for house model
+lock_pins = [17]
+light_pins = [16, 20, 21, 26, 19, 13, 6, 5, 11, 9, 10, 22, 27]
---
+++ example configs/config3.py
@@ -0,0 +1,22 @@
+# light_pins = [4]
+light_pins = [16, 20, 21, 26, 19, 13, 6, 5, 11, 9, 10, 22, 27]
+sensor_array = [
+    [0, 1],
+    [2, 3],
+    [4]
+]
+
+motion_pins = [18, 30]
+motion_light_pins = [3, 31]
+
+KEYPAD = [
+	['1', '2', '3'],
+	['4', '5', '6'],
+	['7', '8', '9'],
+    ['*', '0', '#']
+]
+ROW_PINS = [6, 13, 19, 26]
+COL_PINS = [16, 20, 21]
+keypad_light_pin = 2
+
+lock_pins = [17]
